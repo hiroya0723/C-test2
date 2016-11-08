6b8274642188dee096bb4e1b28fa5ec5e67be3d0
taurin0723 <hiroya0723h@hotmail.co.jp>
2016-11-08 22:41:05 +0900

    uuuu

--- gittest3/gittest3/Program.cs
+++ gittest3/gittest3/Program.cs
@@ -10,7 +10,7 @@ namespace gittest3
     {
         static void Main(string[] args)
         {
-            Console.Write("tedyaetaetyyyyyyyyyy\n");
+            Console.Write("q\n");
         }
     }
 }
